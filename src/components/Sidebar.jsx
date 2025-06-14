31eecc21d6df7c8d76a75144220508fb6f76446a
hibikeyuri <hibikeyuri@gmail.com>
2025-06-14 15:04:29 +0800

    feat: add upload for satellite data to assist development

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import { Uploader } from "@/data/Uploader";
 
 function Sidebar() {
   return (
@@ -8,6 +9,7 @@ function Sidebar() {
       <aside className="bg-grey-0 border-grey-100 row-span-full flex flex-col gap-8 overflow-hidden border-r p-8 px-6">
         <Logo />
         <MainNav />
+        <Uploader></Uploader>
       </aside>
     </>
   );
