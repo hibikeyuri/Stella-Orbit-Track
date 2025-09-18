eb1f8c3ab6c7b940c88f6a7f27d4c3d60715efb1
hibikeyuri <hibikeyuri@gmail.com>
2025-09-19 02:36:24 +0800

    feat(satellite): add image upload support - Updated apiSatellite to handle file upload - Modified table to display satellite image - Extended form with image upload field

--- src/services/apiSatellites.js
+++ src/services/apiSatellites.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getSatellites() {
   const { data: satellites, error } = await supabase
@@ -14,9 +14,15 @@ export async function getSatellites() {
 }
 
 export async function createSatellites(satellite, id) {
+  const imageName = `${Math.random()} - ${satellite.img[0].name}`.replaceAll(
+    "/",
+    "",
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/satellite-images/${imageName}`;
+  // https://huyyxirnkphjevrhsumk.supabase.co/storage/v1/object/public/satellite-images/548301902_1199724078843476_7843780013730249375_n.jpg
   let query = supabase.from("satellites");
   console.log(satellite);
-  if (!id) query = query.insert([satellite]);
+  if (!id) query = query.insert([{ ...satellite, img: imagePath }]);
 
   if (id) {
     console.log("OKOK");
@@ -30,6 +36,17 @@ export async function createSatellites(satellite, id) {
     throw new Error("Satellite cound not be created");
   }
 
+  const { error: storageError } = await supabase.storage
+    .from("satellite-images")
+    .upload(imageName, satellite.img[0]);
+
+  if (storageError) {
+    await supabase.from("satellites").delete().eq("id", satellite.id);
+    console.error(storageError);
+    throw new Error(
+      "Satellite Image cound not be uploaded and the satellite wa not created",
+    );
+  }
   return satellites;
 }
 
